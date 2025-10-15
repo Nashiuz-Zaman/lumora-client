52e3bcfff9e96aab62717185088de2c0b64afa6e
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2025-10-16 04:05:53 +0600

    Add skeletons of approved and reject pages for returns section in admin dashboard

---
+++ src/app/(admin)/admin/returns/approved/page.tsx
@@ -0,0 +1,10 @@
+import { UnderDev } from "@/components/shared";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Approved Return Requests | Admin Panel",
+};
+
+const ApprovedRequestsPage = () => <UnderDev />;
+
+export default ApprovedRequestsPage;
---
+++ src/app/(admin)/admin/returns/rejected/page.tsx
@@ -0,0 +1,10 @@
+import { UnderDev } from "@/components/shared";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Rejected Return Requests | Admin Panel",
+};
+
+const RejectedRequestsPage = () => <UnderDev />;
+
+export default RejectedRequestsPage;
