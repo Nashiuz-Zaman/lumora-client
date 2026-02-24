08e1af2de2ce0f71801449467db7d9874406a5d8
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-02-24 08:46:39 +0600

    remove caching of home page collections data because it's handled by cms

--- src/server-functions/fetchCollectionProducts.ts
+++ src/server-functions/fetchCollectionProducts.ts
@@ -12,7 +12,7 @@ export const fetchCollectionProducts = catchAsyncServer(
     const apiUrl = getBaseApiUrl();
     const res = await fetch(
       `${apiUrl}/products/product-collection/${collectionSlug}`,
-      { next: { revalidate: 5 } }
+      { next: { revalidate: 0 } }
     );
 
     if (!res.ok)
