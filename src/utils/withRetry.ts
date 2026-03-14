17e42e23cc41d4217fe2b6a3dc420121362508e1
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-03-14 07:41:10 +0600

    Fix bug in withRetry util and redeploy

--- src/utils/withRetry.ts
+++ src/utils/withRetry.ts
@@ -3,12 +3,14 @@
  * * @param fn - The async function to execute
  * @param retries - Number of total attempts (default: 3)
  * @param delay - Milliseconds to wait between attempts (default: 1000ms)
+ * @param shouldRetry - boolean flag to determine if the retry should happen
  * @returns The result of the async function
  */
 export const withRetry = async <T>(
   fn: () => Promise<T>,
   retries: number = 3,
   delay: number = 1000,
+  shouldRetry: (error: any) => boolean = () => true,
 ): Promise<T> => {
   try {
     return await fn();
@@ -16,12 +18,14 @@ export const withRetry = async <T>(
     if (retries <= 1) throw error;
 
     // Jitter: a random offset between 0 and 200ms
-    const offset = Math.random() * 200;
+    const offset = Math.random() * 400;
     const totalDelay = delay + offset;
 
     await new Promise((resolve) => setTimeout(resolve, totalDelay));
 
-    console.warn(`Retrying in ${Math.round(totalDelay)}ms...`);
-    return withRetry(fn, retries - 1, delay);
+    console.warn(
+      `Retryable error detected. Retrying in ${Math.round(totalDelay)}ms...`,
+    );
+    return withRetry(fn, retries - 1, delay, shouldRetry);
   }
 };
